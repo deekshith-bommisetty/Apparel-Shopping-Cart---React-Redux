8bc47bccc62b834fe23ae0a41675e64f18ad5d12
deekshith bommisetty <writingtodeek@gmail.com>
2020-08-06 15:09:00 +0530

    create checkout component

--- src/App.js
+++ src/App.js
@@ -10,23 +10,34 @@ class App extends React.Component {
     super();
     this.state = {
       products: data.products,
-      cartItems: [],
-      size:'',
-      sort:''
+      cartItems: JSON.parse(localStorage.getItem('cartItems'))
+        ? JSON.parse(localStorage.getItem('cartItems'))
+        : [],
+      size: '',
+      sort: '',
     };
   }
+  createOrder = (order) => {
+    alert('Need to save order for ' + order.name);
+  };
   removeFromCart = (product) => {
     const cartItems = this.state.cartItems.slice();
     this.setState({
-      cartItems: cartItems.filter(item => item._id !== product._id)
+      cartItems: cartItems.filter((item) => item._id !== product._id),
     });
+    localStorage.setItem(
+      'cartItems',
+      JSON.stringify(
+        this.state.cartItems.filter((item) => item._id !== product._id)
+      )
+    );
   };
 
   // addToCart = (product) => {
   //   const cartItems = this.state.cartItems.slice();
   //   let alreadyInCart = false;
   //   cartItems.forEach((item) => {
-  //     if(item._id === product._id) { 
+  //     if(item._id === product._id) {
   //     item.count++;
   //     alreadyInCart=true;
   //   }
@@ -49,71 +60,77 @@ class App extends React.Component {
       cartItems.push({ ...product, count: 1 });
     }
     this.setState({ cartItems });
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
   };
- sortProducts = (event) => {
-   const sort = event.target.value;
-   console.log(sort);
-   this.setState(state => ({
-     sort: sort,
-     products: this.state.products.sort((a, b) => (
-       sort === 'lowest' ? ((a.price > b.price) ? 1:-1) : sort === 'highest' ? ((a.price < b.price) ? 1:-1) : ((a._id < b._id) ? 1:-1)
-   ))
-   }));
- };
-filterProducts = (event) => {
-  console.log(event.target.value);
-  if(event.target.value === ''){
-    this.setState({
-      size:event.target.value,
-      products: data.products
-    });
-  } else {
-    this.setState({
-      size: event.target.value,
-      products: data.products.filter(
-        (product) => product.availableSizes.indexOf(event.target.value) >= 0 
+  sortProducts = (event) => {
+    const sort = event.target.value;
+    console.log(sort);
+    this.setState((state) => ({
+      sort: sort,
+      products: this.state.products.sort((a, b) =>
+        sort === 'lowest'
+          ? a.price > b.price
+            ? 1
+            : -1
+          : sort === 'highest'
+          ? a.price < b.price
+            ? 1
+            : -1
+          : a._id < b._id
+          ? 1
+          : -1
       ),
-    });
-  }
-}
-
+    }));
+  };
+  filterProducts = (event) => {
+    console.log(event.target.value);
+    if (event.target.value === '') {
+      this.setState({
+        size: event.target.value,
+        products: data.products,
+      });
+    } else {
+      this.setState({
+        size: event.target.value,
+        products: data.products.filter(
+          (product) => product.availableSizes.indexOf(event.target.value) >= 0
+        ),
+      });
+    }
+  };
   render() {
     return (
-      <div className= 'grid-container'>
+      <div className='grid-container'>
         <header>
           <a href='/'> React Shopping Cart </a>
         </header>
         <main>
           <div className='content'>
-          <div className='main'>
-            <Filter 
-              count={this.state.products.length}
-              size={this.state.size}
-              sort={this.state.sort}
-              filterProducts={this.filterProducts}
-              sortProducts={this.sortProducts}
+            <div className='main'>
+              <Filter
+                count={this.state.products.length}
+                size={this.state.size}
+                sort={this.state.sort}
+                filterProducts={this.filterProducts}
+                sortProducts={this.sortProducts}
               />
-            <Products 
-              products={this.state.products}
-              addToCart={this.addToCart}
-
-            />
+              <Products
+                products={this.state.products}
+                addToCart={this.addToCart}
+              />
+            </div>
+            <div className='sidebar'>
+              <Cart
+                cartItems={this.state.cartItems}
+                removeFromCart={this.removeFromCart}
+                createOrder={this.createOrder}
+              />
+            </div>
           </div>
-          <div className='sidebar'>
-              <Cart cartItems={this.state.cartItems} 
-              removeFromCart= {this.removeFromCart} />
-          </div>
-          </div>
-          
         </main>
-        <footer>
-          All Rights Reserved
-        </footer>
+        <footer>All Rights Reserved</footer>
       </div>
     );
   }
 }
-
 export default App;
-
-
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,32 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import formatCurrency from '../util';
 
 export default class Products extends Component {
-    render() {
-        return (
-            <div>
-                <ul className='products'>
-                    {this.props.products.map(product =>
-                     <li key={product._id}>
-                        <div className='product'>
-                            <a href= {'#' + product._id}>
-                                <img src={product.image} alt={product.titile}></img>
-                                <p>{product.title}</p>
-                            </a>
-                            <div className='product-price'>
-                                <div>{formatCurrency(product.price)}</div>
-                                <button className='button primary' onClick={() => this.props.addToCart(product)}>Add to Cart</button>
-                            </div>
-                        </div>
-                    </li>)}
-                </ul> 
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <ul className='products'>
+          {this.props.products.map((product) => (
+            <li key={product._id}>
+              <div className='product'>
+                <a href={'#' + product._id}>
+                  <img src={product.image} alt={product.titile}></img>
+                  <p>{product.title}</p>
+                </a>
+                <div className='product-price'>
+                  <div>{formatCurrency(product.price)}</div>
+                  <button
+                    className='button primary'
+                    onClick={() => this.props.addToCart(product)}
+                  >
+                    Add to Cart
+                  </button>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
-
-
-
-
-
-
